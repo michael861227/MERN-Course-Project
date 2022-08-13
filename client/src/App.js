2d94b2f19967eee6cd2bb30e14d6f513826cbb90
Michael Chou <michael861227@gmail.com>
2022-08-14 00:31:50 +0800

    Add state in register-component.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomeComponent from "./components/home-component";
 import NavComponent from "./components/nav-component";
+import RegisterComponent from "./components/register-component";
+import ResisterComponent from "./components/register-component";
 
 function App() {
   return (
     <div>
       <NavComponent />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/register" element={<RegisterComponent />} />
       </Routes>
-      <HomeComponent />
     </div>
   );
 }
