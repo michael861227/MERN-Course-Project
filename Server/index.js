60f7e843343e315605deda9c3b4232914ffb171f
Michael Chou <michael861227@gmail.com>
2022-08-13 17:03:25 +0800

    Initial setup for server

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// connect to MongoDB
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect to Mongo Altas");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080.");
+});
