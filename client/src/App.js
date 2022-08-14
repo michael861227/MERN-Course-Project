df7174288a31c06c7465f84739541b284b95537e
Michael Chou <michael861227@gmail.com>
2022-08-14 12:22:34 +0800

    Update with conditional nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomeComponent from "./components/home-component";
 import NavComponent from "./components/nav-component";
 import RegisterComponent from "./components/register-component";
 import LoginComponent from "./components/login-component";
+import ProfileComponent from "./components/profile-component";
+import AuthService from "./services/auth.service";
 
 function App() {
+  let [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser);
+
   return (
     <div>
-      <NavComponent />
+      <NavComponent currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/register" element={<RegisterComponent />} />
-        <Route path="/login" element={<LoginComponent />} />
+        <Route
+          path="/login"
+          element={
+            <LoginComponent
+              currentUser={currentUser}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProfileComponent
+              currentUser={currentUser}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        />
       </Routes>
     </div>
   );
